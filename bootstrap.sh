836ffb9423d6aa57ce3ac1e47a4630dfa688bf26
Usman Bashir <me@usmanbashir.com>
2016-05-31 15:03:59 +0300

    Add setup code for Tmux configuration.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,19 +3,28 @@ echo "Creating symbolic links..."
 
 
 # If .bashrc or .bash_profile already exists
-# and are a regular file, then back them up
+# and are regular files, then back them up
 # for just in case.
 if [ -f ~/.bashrc ]; then
-    mv ~/.bashrc ~/.bashrc.bk
+  mv ~/.bashrc ~/.bashrc.bk
 fi
 
 if [ -f ~/.bash_profile ]; then
-    mv ~/.bash_profile ~/.bash_profile.bk
+  mv ~/.bash_profile ~/.bash_profile.bk
 fi
 
 
 ln -s ~/dotfiles/bash/bashrc ~/.bashrc
 ln -s ~/dotfiles/bash/bashrc ~/.bash_profile
+
+
+# If .tmux.conf already exists
+# then back them up just in case.
+if [ -f ~/.tmux.conf ]; then
+  mv ~/.tmux.conf ~/.tmux.conf.bk
+fi
+
+ln -s ~/dotfiles/tmux/conf ~/.tmux.conf
 
 
 echo "All done for the day!"
